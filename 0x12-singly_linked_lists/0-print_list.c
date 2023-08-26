efab950fa269664981195cdec2d0f0393eb5c6ae
hamzaAlaoui94 <hz.alaouita@gmail.com>
2023-08-25 18:11:38 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 	size_t s = 0;
 
 	while (h)
- {
+	{
 
 	 if (!h->str)
 
@@ -20,11 +20,12 @@ size_t print_list(const list_t *h)
 
 	 else
 
+
 		 printf("[%u] %s\n", h->len, h->str);
 	 h = h->next;
 
 	 s++;
- }
+	}
 
 	return (s);
 }
